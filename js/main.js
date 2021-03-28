7bcbb5d6e789f13d5c22546a653a00180555ec61
Matt McBrien <mmcbrien@amazon.com>
2021-03-27 23:44:43 -0400

    Clean up main js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 var myHeading = document.getElementById("welcomeMessage");
-console.log("hello from js");
-var geocoder;
-
-
-
+console.log(
+"_______                           \n" +
+"|__    \\                          \n" + 
+"   \\    \\                         \n" +
+"    \\    \\                        \n" +
+"    /     \\                       \n" +
+"   /  /\\   \\                      \n" +
+"  /  /  \\   \\_                    \n" +
+" /__/    \\____|                   \n" 
+);
 $(document).ready( function() { 
-    console.log("Getting location");
+    console.log("Accessing location via 'https://geoip-db.com/json/'");
     $.getJSON("https://geoip-db.com/json/",
         function (result) {
             if (result.length !== 0) {
-		console.log("Got location: " + result.city);
                 myHeading.textContent = "Thanks for visiting my website from " + result.city + ".";
             }
         });
