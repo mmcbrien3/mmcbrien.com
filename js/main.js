70ec6c685f22964b92e8bafb965bb1cd39b1b56d
Matt McBrien <mmcbrien@amazon.com>
2023-04-02 12:55:43 -0700

    Add github commit info proof of concept
    This adds the Git commits from Git onto the main
    page of the website. The main issue right now is
    I'm not authenticating the requests so the rate
    limit is quite low and I'm getting throttled frequently.
    When the request fails (e.g. throttled), I remove the
    table.

--- js/main.js
+++ js/main.js
@@ -8,20 +8,43 @@ console.log(
 "  /  /  \\   \\_                    \n" +
 " /__/    \\____|                   \n" 
 );
-var messageElement = document.getElementById("welcomeMessage");
+const MAX_COMMITS_TO_SHOW = 3;
+var commitTitles = document.getElementsByClassName("commitTableEntry");
+var gitTable = document.getElementById('gitCommitsTable');
+// TODO: add token to increase throttling limit. Getting throttled a lot right now.
 $(document).ready(() => { 
-    console.log("Accessing location via 'https://geoip-db.com/json/'");
     $.ajax({
-        url: "https://geoip-db.com/json/",  
-        dataType: 'jsonp',
+        url: "https://api.github.com/repos/mmcbrien3/mmcbrien.com/commits",
+        headers: { 'User-Agent': 'mmcbrien.com' },
+        dataType: 'json',
         success: (result) => {
-	    let welcoming = "Thanks for visiting my website from ";
-	    let city = "Planet Earth";
-            if (result.length !== 0) {
-                city = result.city;
-            }
-	    welcoming += city;
-	    messageElement.textContent = welcoming;
+            console.log(`Found ${result.length} commits`);
+            let commitsDisplayed = 0;
+            let currentElement = commitTitles[0];
+            while (commitsDisplayed < MAX_COMMITS_TO_SHOW && result.length >= commitsDisplayed) {
+                let currentCommit = result[commitsDisplayed];
+                let messageEntry = currentElement.appendChild(document.createElement('th'));
+                messageEntry.textContent = getFirstLineOfCommit(currentCommit.commit.message);
+                messageEntry.style.fontSize = 'x-small';
+                let shaEntry = currentElement.appendChild(document.createElement('th'));
+                linkElement = shaEntry.appendChild(document.createElement('a'));
+                linkElement.href = currentCommit.html_url;
+                linkElement.textContent = currentCommit.sha.substring(0, 7);
+                shaEntry.style.fontSize = 'x-small';
+                commitsDisplayed += 1;
+                let clonedNode = currentElement.cloneNode();
+                currentElement.parentElement.append(document.createElement('p'));
+                currentElement.parentElement.appendChild(clonedNode);
+                currentElement = clonedNode;
+            };
+        },
+        error: (error) => {
+            gitTable.remove();
         }
-    });
+    })
 });
+
+function getFirstLineOfCommit(commitMessage) {
+    var lines = commitMessage.split("\n");   // split all lines into array
+    return lines.shift();   
+}
